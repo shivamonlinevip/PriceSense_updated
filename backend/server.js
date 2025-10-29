9fd336a282b8739fd044df5a8277b838ba06aaba
shivamonlinevip <shivamonline.vip@gmail.com>
2025-10-30 01:29:10 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const priceRoutes = require('./routes/price');
-const app = express();
-app.use(bodyParser.json());
-app.use('/api', priceRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log('Server running on', PORT));
