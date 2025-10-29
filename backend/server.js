e85ed3cc7307247c058d1ed1a1faf9ff900cd2c4
shivamonlinevip <shivamonline.vip@gmail.com>
2025-10-29 15:53:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,8 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
+const priceRoutes = require('./routes/price');
 const app = express();
-const port = process.env.PORT || 10000;
-
-// parse JSON
-app.use(express.json());
-
-// API route
-app.use('/api/price', require('./routes/price'));
-
-// Serve frontend
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// catch-all route to serve React frontend
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.use(bodyParser.json());
+app.use('/api', priceRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log('Server running on', PORT));
